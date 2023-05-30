563de219967e1379c956f8045380111842d18cc1
Pablo <jplopez@scl.edu.gt>
2023-05-29 21:36:35 -0600

    Se agrego la base de datos

---
+++ js/data.js
@@ -0,0 +1,70 @@
+let data_todo_list = [
+
+    {
+        id : "1",
+        Nomnre : "Mesa",
+        Categoria : "Comedor",
+        estado: true
+    },
+    {
+        id : "2",
+        Nomnre : "Bancos",
+        Categoria : "Comedor",
+        estado: true
+    },
+    {
+        id : "3",
+        Nomnre : "Tinas",
+        Categoria : "Baño",
+        estado: true
+    },
+    {
+        id : "4",
+        Nomnre : "llaves",
+        Categoria : "Baño",
+        estado: true
+    },
+    {
+        id : "5",
+        Nomnre : "Fluxometro",
+        Categoria : "Baño",
+        estado: true
+    },
+    {
+        id : "6",
+        Nomnre : "asiento",
+        Categoria : "Baño",
+        estado: true
+    },
+    {
+        id : "7",
+        Nomnre : "tarjetas",
+        Categoria : "Baño",
+        estado: true
+    },
+    {
+        id : "8",
+        Nomnre : "regadera",
+        Categoria : "Baño",
+        estado: true
+    },
+    {
+        id : "9",
+        Nomnre : "Inodoro",
+        Categoria : "Baño",
+        estado: true
+    },
+    {
+        id : "10",
+        Nomnre : "Mesa de centro",
+        Categoria : "Sala",
+        estado: true
+    },
+    {
+        id : "11",
+        Nomnre : "Sofá",
+        Categoria : "Sala",
+        estado: true
+    },
+ 
+ ];
